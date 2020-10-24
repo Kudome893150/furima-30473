eb6ccc747a645e62d642e46191e3696e2f2326ea
Kudome893150 <71761715+Kudome893150@users.noreply.github.com>
2020-10-24 10:45:06 +0900

    商品情報編集機能　修正#2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except:[:index, :show]
-  before_action :item_method, only:[:show, :edit, :update,]
+  before_action :item_method, only:[:show, :edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC').includes(:user)
