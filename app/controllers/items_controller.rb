51feb3f2d0bbf61d82044a3f09d5bfef4d46da94
Kudome893150 <71761715+Kudome893150@users.noreply.github.com>
2020-10-24 09:43:35 +0900

    商品情報編集機能　修正#1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except:[:index, :show]
+  before_action :item_method, only:[:show, :edit, :update,]
 
   def index
     @items = Item.all.order('created_at DESC').includes(:user)
@@ -20,18 +21,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
      unless current_user == @item.user
        redirect_to root_path
      end
   end
 
   def update 
-    @item = Item.find(params[:id])
       if @item.update(item_params)
         redirect_to item_path(@item)
       else
@@ -44,4 +42,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:comment, :category_id, :condition_id, :name, :charge_id, :area_id, :delivery_date_id, :price, :image).merge(user_id: current_user.id)
   end
+
+  def item_method
+    @item = Item.find(params[:id])
+  end
+
 end
